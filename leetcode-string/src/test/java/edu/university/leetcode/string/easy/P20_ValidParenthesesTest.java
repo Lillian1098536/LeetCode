5551285c2023a2c1a4807d2b9263cde7eed40211
lillian <zheng.lillian@gmail.com>
2017-11-19 15:03:24 -0500

    valid parentheses

---
+++ leetcode-string/src/test/java/edu/university/leetcode/string/easy/P20_ValidParenthesesTest.java
@@ -0,0 +1,33 @@
+package edu.university.leetcode.string.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class P20_ValidParenthesesTest {
+
+    @Test
+    public void isValid1() throws Exception {
+        boolean result = P20_ValidParentheses.isValid("()");
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid2() throws Exception {
+        boolean result = P20_ValidParentheses.isValid("()[]{}");
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid3() throws Exception {
+        boolean result = P20_ValidParentheses.isValid("(]");
+        assertTrue(!result);
+    }
+
+    @Test
+    public void isValid4() throws Exception {
+        boolean result = P20_ValidParentheses.isValid("([)]");
+        assertTrue(!result);
+    }
+
+}
