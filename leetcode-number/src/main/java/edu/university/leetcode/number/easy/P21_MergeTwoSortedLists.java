f18de8790d2b6d0360746e4f2b7c308e8bd824f9
lillian <zheng.lillian@gmail.com>
2017-11-20 18:01:31 -0500

    merge two sorted lists more

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/P21_MergeTwoSortedLists.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P21_MergeTwoSortedLists.java
@@ -39,6 +39,23 @@ public class P21_MergeTwoSortedLists {
         return head.next;
     }
 
+    public static ListNode mergeTwoSortedLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.value < l2.value) {
+            l1.next = mergeTwoSortedLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoSortedLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
     @Data
     public static class ListNode {
 
--- leetcode-number/src/test/java/edu/university/leetcode/number/easy/P21_MergeTwoSortedListsTest.java
+++ leetcode-number/src/test/java/edu/university/leetcode/number/easy/P21_MergeTwoSortedListsTest.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.*;
 import static edu.university.leetcode.number.easy.P21_MergeTwoSortedLists.ListNode;
 
 public class P21_MergeTwoSortedListsTest {
+
     @Test
-    public void mergeTwoLists() throws Exception {
+    public void mergeTwoLists1() throws Exception {
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(4);
         l1.next.next = new ListNode(8);
@@ -21,6 +22,24 @@ public class P21_MergeTwoSortedListsTest {
         expected.next.next.next.next = new ListNode(7);
         expected.next.next.next.next.next = new ListNode(8);
         ListNode result = P21_MergeTwoSortedLists.mergeTwoLists(l1, l2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mergeTwoLists2() throws Exception {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(8);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(5);
+        l2.next.next = new ListNode(7);
+        ListNode expected = new ListNode(1);
+        expected.next = new ListNode(2);
+        expected.next.next = new ListNode(4);
+        expected.next.next.next = new ListNode(5);
+        expected.next.next.next.next = new ListNode(7);
+        expected.next.next.next.next.next = new ListNode(8);
+        ListNode result = P21_MergeTwoSortedLists.mergeTwoSortedLists(l1, l2);
         assertEquals(expected, result);
     }
 
