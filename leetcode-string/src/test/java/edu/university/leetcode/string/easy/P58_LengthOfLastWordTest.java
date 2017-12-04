56365e26926ad03f4136b24ae329a2c5260a1106
lillian <zheng.lillian@gmail.com>
2017-12-04 16:32:07 -0500

    length of last word more

--- leetcode-string/src/test/java/edu/university/leetcode/string/easy/P58_LengthOfLastWordTest.java
+++ leetcode-string/src/test/java/edu/university/leetcode/string/easy/P58_LengthOfLastWordTest.java
@@ -7,8 +7,14 @@ import static org.junit.Assert.*;
 public class P58_LengthOfLastWordTest {
 
     @Test
-    public void lengthOfLastWord() throws Exception {
+    public void lengthOfLastWord1() throws Exception {
         int result = P58_LengthOfLastWord.lengthOfLastWord("Hello World");
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void lengthOfLastWord2() throws Exception {
+        int result = P58_LengthOfLastWord.lengthOfLastWord("Hello World ");
         assertEquals(5, result);
     }
 
