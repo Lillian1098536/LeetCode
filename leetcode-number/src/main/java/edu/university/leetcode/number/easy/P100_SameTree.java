57b60a41f275072a81e01c50f7dfb90d3f6367ea
lillian <zheng.lillian@gmail.com>
2018-01-04 15:39:40 -0500

    simplify same tree

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/P100_SameTree.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P100_SameTree.java
@@ -8,17 +8,10 @@ package edu.university.leetcode.number.easy;
 public class P100_SameTree {
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
         if (p == null || q == null) {
-            return false;
+            return p == null && q == null;
         }
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else {
-            return false;
-        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
     public static class TreeNode {
