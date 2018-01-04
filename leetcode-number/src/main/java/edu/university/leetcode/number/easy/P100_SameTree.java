caf810dbae9734d5340774bc4367f7775fc2f9cc
lillian <zheng.lillian@gmail.com>
2018-01-04 15:25:53 -0500

    same tree

---
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P100_SameTree.java
@@ -0,0 +1,36 @@
+package edu.university.leetcode.number.easy;
+
+/**
+ * 100. Same Tree
+ * Given two binary trees, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ */
+public class P100_SameTree {
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+
+    }
+
+}
