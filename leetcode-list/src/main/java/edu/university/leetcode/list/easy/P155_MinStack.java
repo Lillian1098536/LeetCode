f054f72a07dbc0ae48098fd56624e1f1afa3bfa2
lillian <zheng.lillian@gmail.com>
2018-04-20 22:13:42 -0700

    min stack

---
+++ leetcode-list/src/main/java/edu/university/leetcode/list/easy/P155_MinStack.java
@@ -0,0 +1,43 @@
+package edu.university.leetcode.list.easy;
+
+import java.util.Stack;
+
+/**
+ * 155. Min Stack
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ */
+public class P155_MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public P155_MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.empty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (minStack.peek().equals(stack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
