63818f1134f7b729b44712d864761b7310c2e31a
lillian <zheng.lillian@gmail.com>
2018-04-22 18:52:18 -0700

    two sun two sorted array

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/P1_TwoSum.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P1_TwoSum.java
@@ -16,6 +16,11 @@ import java.util.HashMap;
 public class P1_TwoSum {
 
     /**
+     * 1. Two Sum
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * Example: Given nums = [2, 7, 11, 15], target = 9,
+     * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
      * Approach: One-pass Hash Table
      * Complexity Analysis
      * Time complexity : O(n). Traverse the list containing n elements only once. Each look up in the table costs only O(1) time.
