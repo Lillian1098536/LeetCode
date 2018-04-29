92d194b9852aab022ceaddc1652cd60415cc368e
lillian <zheng.lillian@gmail.com>
2018-04-29 16:38:51 -0700

    house robber

---
+++ leetcode-number/src/test/java/edu/university/leetcode/number/easy/P198_HouseRobberTest.java
@@ -0,0 +1,20 @@
+package edu.university.leetcode.number.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P198_HouseRobberTest {
+    @Test
+    public void rob1() throws Exception {
+        int result = P198_HouseRobber.rob(new int [] {2,7,9,3,1});
+        assertEquals(12, result);
+    }
+
+    @Test
+    public void rob2() throws Exception {
+        int result = P198_HouseRobber.robHouse(new int [] {2,7,9,3,1});
+        assertEquals(12, result);
+    }
+
+}
