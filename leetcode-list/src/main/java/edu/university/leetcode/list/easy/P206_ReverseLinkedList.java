0ea16ac2259b966e7db7064c6b65a2f076d5f70b
lillian <zheng.lillian@gmail.com>
2018-04-30 22:16:59 -0700

    reverse linked list

---
+++ leetcode-list/src/main/java/edu/university/leetcode/list/easy/P206_ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package edu.university.leetcode.list.easy;
+
+import static edu.university.leetcode.list.medium.P2_AddTwoNumbers.ListNode;
+
+/**
+ * 206. Reverse Linked List
+ * Reverse a singly linked list.
+ * Example:
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ */
+public class P206_ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = node;
+            node = head;
+            head = tmp;
+        }
+        return node;
+    }
+
+}
