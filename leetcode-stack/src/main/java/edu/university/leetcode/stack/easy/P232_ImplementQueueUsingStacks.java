9734cafdf6734db83533079b2c60334a3aae4a9c
lillian <zheng.lillian@gmail.com>
2018-05-01 21:00:27 -0700

    implement queue using stacks

---
+++ leetcode-stack/src/main/java/edu/university/leetcode/stack/easy/P232_ImplementQueueUsingStacks.java
@@ -0,0 +1,69 @@
+package edu.university.leetcode.stack.easy;
+
+import java.util.Stack;
+
+/**
+ * 232. Implement Queue using Stacks
+ * Implement the following operations of a queue using stacks.
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+ * Notes: You must use only standard operations of a stack
+ * which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ * Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque
+ * (double-ended queue), as long as you use only standard operations of a stack.
+ * You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
+public class P232_ImplementQueueUsingStacks {
+
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public P232_ImplementQueueUsingStacks() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        in.add(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+
+    public boolean empty() {
+        return in.empty() && out.empty();
+    }
+}
