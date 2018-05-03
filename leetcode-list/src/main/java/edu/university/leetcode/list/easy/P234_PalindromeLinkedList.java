f4e106d298b3eb2a36e94ff0f1a153ce1a63e4d8
lillian <zheng.lillian@gmail.com>
2018-05-02 21:49:23 -0700

    palindrome linked list

---
+++ leetcode-list/src/main/java/edu/university/leetcode/list/easy/P234_PalindromeLinkedList.java
@@ -0,0 +1,30 @@
+package edu.university.leetcode.list.easy;
+
+import static edu.university.leetcode.list.medium.P2_AddTwoNumbers.ListNode;
+
+/**
+ * 234. Palindrome Linked List
+ * Given a singly linked list, determine if it is a palindrome.
+ * O(n) time and O(1) space
+ */
+public class P234_PalindromeLinkedList {
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null && head.next == null) return true;
+        ListNode tmpHead = head, reverse = new ListNode(head.val);
+        while (tmpHead.next != null) {
+            tmpHead = tmpHead.next;
+            ListNode tmp = new ListNode(tmpHead.val);
+            tmp.next = reverse;
+            reverse = tmp;
+        }
+        if (head.val != reverse.val) return false;
+        while (head.next != null) {
+            head = head.next;
+            reverse = reverse.next;
+            if (head.val != reverse.val) return false;
+        }
+        return true;
+    }
+
+}
---
+++ leetcode-list/src/test/java/edu/university/leetcode/list/easy/P234_PalindromeLinkedListTest.java
@@ -0,0 +1,19 @@
+package edu.university.leetcode.list.easy;
+
+import org.junit.Test;
+
+import static edu.university.leetcode.list.medium.P2_AddTwoNumbers.ListNode;
+import static org.junit.Assert.assertTrue;
+
+public class P234_PalindromeLinkedListTest {
+    @Test
+    public void isPalindrome() throws Exception {
+        ListNode test = new ListNode(1);
+        test.next = new ListNode(2);
+        test.next.next = new ListNode(3);
+        test.next.next.next = new ListNode(2);
+        test.next.next.next.next = new ListNode(1);
+        assertTrue(P234_PalindromeLinkedList.isPalindrome(test));
+    }
+
+}
