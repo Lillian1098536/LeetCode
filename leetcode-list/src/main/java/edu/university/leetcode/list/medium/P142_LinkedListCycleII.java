f18f262a7c9ce1727f0a28416dce304efc3300f2
lillian <zheng.lillian@gmail.com>
2018-06-04 16:07:18 -0700

    linked list cycle II

---
+++ leetcode-list/src/main/java/edu/university/leetcode/list/medium/P142_LinkedListCycleII.java
@@ -0,0 +1,27 @@
+package edu.university.leetcode.list.medium;
+
+import static edu.university.leetcode.list.medium.P2_AddTwoNumbers.ListNode;
+/**
+ * 142. Linked List Cycle II
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ */
+public class P142_LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode tmp = head;
+                while(tmp != slow){
+                    tmp = tmp.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+}
