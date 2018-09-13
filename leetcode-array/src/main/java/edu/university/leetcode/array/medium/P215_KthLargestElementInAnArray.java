7be759e891484058938ef2564cd132762d8571dd
lillian <zheng.lillian@gmail.com>
2018-09-12 20:31:34 -0700

    find kth largest

---
+++ leetcode-array/src/main/java/edu/university/leetcode/array/medium/P215_KthLargestElementInAnArray.java
@@ -0,0 +1,19 @@
+package edu.university.leetcode.array.medium;
+
+import java.util.Arrays;
+
+/**
+ * 215. Kth Largest Element in an Array
+ * Find the kth largest element in an unsorted array.
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * Example 1: Input: [3,2,1,5,6,4] and k = 2 Output: 5
+ * Example 2: Input: [3,2,3,1,2,4,5,5,6] and k = 4 Output: 4
+ */
+public class P215_KthLargestElementInAnArray {
+
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+}
