bc3e71c1bae7e41d5a4c8c8c7a12442d49315a10
lillian <zheng.lillian@gmail.com>
2019-04-11 20:29:59 -0700

    383. Ransom Note

---
+++ leetcode-string/src/main/java/edu.university.leetcode.string/easy/P383_RansomNote.java
@@ -0,0 +1,28 @@
+package edu.university.leetcode.string.easy;
+
+/**
+ * 383. Ransom Note
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+ * Each letter in the magazine string can only be used once in your ransom note.
+ * Note: You may assume that both strings contain only lowercase letters.
+ * canConstruct("a", "b") -> false
+ * canConstruct("aa", "ab") -> false
+ * canConstruct("aa", "aab") -> true
+ */
+public class P383_RansomNote {
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        for (char c : magazine.toCharArray()) {
+            letters[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            int count = --letters[c - 'a'];
+            if (count < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ leetcode-string/src/test/java/edu/university/leetcode/string/easy/P383_RansomNoteTest.java
@@ -0,0 +1,13 @@
+package edu.university.leetcode.string.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P383_RansomNoteTest {
+
+    @Test
+    public void canConstruct() {
+        assertTrue(!P383_RansomNote.canConstruct("aa", "ab"));
+    }
+}
