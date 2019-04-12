3a92a377e15c90c5f45f894e049d41ac38356871
lillian <zheng.lillian@gmail.com>
2019-04-11 21:09:38 -0700

    389. Find the Difference

---
+++ leetcode-string/src/test/java/edu/university/leetcode/string/easy/P389_FindTheDifferenceTest.java
@@ -0,0 +1,14 @@
+package edu.university.leetcode.string.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P389_FindTheDifferenceTest {
+
+    @Test
+    public void findTheDifference() {
+        assertEquals('e', P389_FindTheDifference.findTheDifference("abcd", "abcde"));
+    }
+
+}
