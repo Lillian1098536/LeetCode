9fd69b51f87f63db17d9eae03c13f619bf68448b
lillian <zheng.lillian@gmail.com>
2019-04-14 12:37:21 -0700

    693. Binary Number with Alternating Bits

---
+++ leetcode-binary/src/test/java/edu/university/leetcode/binary/easy/P693_BinaryNumberWithAlternatingBitsTest.java
@@ -0,0 +1,13 @@
+package edu.university.leetcode.binary.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P693_BinaryNumberWithAlternatingBitsTest {
+
+    @Test
+    public void hasAlternatingBits() {
+        assertTrue(P693_BinaryNumberWithAlternatingBits.hasAlternatingBits(5));
+    }
+}
