947841941015a02ef62d89128319d7f0cee55355
lillian <zheng.lillian@gmail.com>
2019-04-22 21:58:55 -0700

    276. Paint Fence

---
+++ leetcode-dynamic-programming/src/main/java/edu/university/leetcode/dynamicprogramming/easy/P276_PaintFence.java
@@ -0,0 +1,23 @@
+package edu.university.leetcode.dynamicprogramming.easy;
+
+/**
+ * 276. Paint Fence
+ * There is a fence with n posts, each post can be painted with one of the k colors.
+ * You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+ * Return the total number of ways you can paint the fence.
+ * Note: n and k are non-negative integers.
+ */
+public class P276_PaintFence {
+
+    public static int numWays(int n, int k) {
+        if (n == 0 || k == 0) return 0;
+        if (n == 1) return k;
+        int same = k, diff = k * (k -1), res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = same + diff;
+            same = diff;
+            diff = res * (k - 1);
+        }
+        return res;
+    }
+}
---
+++ leetcode-dynamic-programming/src/test/java/edu/university/leetcode/dynamicprogramming/easy/P276_PaintFenceTest.java
@@ -0,0 +1,14 @@
+package edu.university.leetcode.dynamicprogramming.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P276_PaintFenceTest {
+
+    @Test
+    public void numWays() {
+        assertEquals(9, P276_PaintFence.numWays(2, 3));
+    }
+
+}
