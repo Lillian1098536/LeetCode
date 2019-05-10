2295a51a99c676cdbdd57777351f6382836fb2f8
lillian <zheng.lillian@gmail.com>
2019-05-09 20:26:57 -0700

    476. Number Complement

---
+++ leetcode-binary/src/test/java/edu/university/leetcode/binary/easy/P476_NumberComplementTest.java
@@ -0,0 +1,14 @@
+package edu.university.leetcode.binary.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P476_NumberComplementTest {
+
+    @Test
+    public void findComplement() {
+        assertEquals(2, P476_NumberComplement.findComplement(5));
+    }
+
+}
