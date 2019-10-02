bf8092ce609e07226fff84411d6dabb3f0db80a7
lillian <zheng.lillian@gmail.com>
2019-10-01 22:29:42 -0700

    log4j2

---
+++ leetcode-tree/src/test/java/edu/university/leetcode/tree/medium/P98_ValidateBinarySearchTreeTest.java
@@ -0,0 +1,12 @@
+package edu.university.leetcode.tree.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P98_ValidateBinarySearchTreeTest {
+
+    @Test
+    public void isValidBST() {
+    }
+}
