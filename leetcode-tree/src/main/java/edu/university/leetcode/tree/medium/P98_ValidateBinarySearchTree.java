db98a616a7033f0c14a0479e2fd59f122d82cecf
lillian <zheng.lillian@gmail.com>
2019-10-02 22:39:15 -0700

    98. Validate Binary Search Tree

--- leetcode-tree/src/main/java/edu/university/leetcode/tree/medium/P98_ValidateBinarySearchTree.java
+++ leetcode-tree/src/main/java/edu/university/leetcode/tree/medium/P98_ValidateBinarySearchTree.java
@@ -1,5 +1,8 @@
 package edu.university.leetcode.tree.medium;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static edu.university.leetcode.tree.easy.P100_SameTree.TreeNode;
 
 /**
@@ -10,30 +13,56 @@ import static edu.university.leetcode.tree.easy.P100_SameTree.TreeNode;
  * The right subtree of a node contains only nodes with keys greater than the node's key.
  * Both the left and right subtrees must also be binary search trees.
  * Example 1: Input:
- *   2
- *  / \
- * 1   3
+ * 8
+ * / \
+ * 3   10
+ * / \    \
+ * 1   6    14
+ * / \  /
+ * 4  7 13
  * Output: true
  * Example 2: Input:
- *     5
- *    / \
- *   1   4
- *  / \
+ * 5
+ * / \
+ * 1   4
+ * / \
  * 3   6
- Output: false
- Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
- is 5 but its right child's value is 4.
+ * Output: false
+ * Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+ * is 5 but its right child's value is 4.
  */
+
 public class P98_ValidateBinarySearchTree {
 
+    private static Logger LOG = LoggerFactory.getLogger(P98_ValidateBinarySearchTree.class);
+
     private TreeNode lastNode;
+
     public boolean isValidBST(TreeNode root) {
+        if (root != null) {
+            LOG.debug("node value = {}", root.val);
+
+        }
         if (root == null) return true;
         if (!isValidBST(root.left)) return false;
         if (lastNode != null && lastNode.val >= root.val) return false;
         lastNode = root;
         if (!isValidBST(root.right)) return false;
         return true;
+    }
+
+    public boolean isValidBST2(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean dfs(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        else if (node != null) {
+            LOG.debug("node value = {}", node.val);
+        }
+        return node.val > min && node.val < max && dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
     }
 
 }
--- leetcode-tree/src/test/java/edu/university/leetcode/tree/medium/P98_ValidateBinarySearchTreeTest.java
+++ leetcode-tree/src/test/java/edu/university/leetcode/tree/medium/P98_ValidateBinarySearchTreeTest.java
@@ -1,12 +1,77 @@
 package edu.university.leetcode.tree.medium;
 
+import static edu.university.leetcode.tree.easy.P100_SameTree.TreeNode;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Example 1: Input:
+ *     8
+ *    / \
+ *   3   10
+ *  / \    \
+ * 1   6    14
+ *    / \  /
+ *   4  7 13
+ * Output: true
+ * Example 2: Input:
+ *     5
+ *    / \
+ *   1   4
+ *  / \
+ * 3   6
+ Output: false
+ */
 public class P98_ValidateBinarySearchTreeTest {
 
+    P98_ValidateBinarySearchTree tree = new P98_ValidateBinarySearchTree();
+
     @Test
-    public void isValidBST() {
+    public void isValidBSTtrue() {
+        assertTrue(tree.isValidBST(getTrueTree()));
     }
+
+    @Test
+    public void isValidBSTtrue2() {
+        assertTrue(tree.isValidBST2(getTrueTree()));
+    }
+
+    @Test
+    public void isValidBSTfalse() {
+        assertTrue(!tree.isValidBST(getFlastTree()));
+    }
+
+    @Test
+    public void isValidBSTfalse2() {
+        assertTrue(!tree.isValidBST2(getFlastTree()));
+    }
+
+    private TreeNode getTrueTree() {
+        TreeNode t6 = new TreeNode(6);
+        t6.left = new TreeNode(4);
+        t6.right = new TreeNode(7);
+        TreeNode t3 = new TreeNode(3);
+        t3.left = new TreeNode(1);
+        t3.right = t6;
+        TreeNode t8 = new TreeNode(8);
+        t8.left = t3;
+        TreeNode t10 = new TreeNode(10);
+        TreeNode t14 = new TreeNode(14);
+        t10.right = t14;
+        t14.left = new TreeNode(13);
+        t8.right = t10;
+        return t8;
+    }
+
+    private TreeNode getFlastTree() {
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(3);
+        t1.right = new TreeNode(6);
+        TreeNode t5 = new TreeNode(5);
+        t5.left = t1;
+        t5.right = new TreeNode(4);
+        return t5;
+    }
+
 }
