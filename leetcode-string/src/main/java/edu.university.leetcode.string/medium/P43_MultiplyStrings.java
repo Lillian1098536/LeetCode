09b40258352ba802913ddefca10f6de907650411
lillian <zheng.lillian@gmail.com>
2019-11-13 18:51:25 -0800

    43. Multiply Strings

--- leetcode-string/src/main/java/edu.university.leetcode.string/medium/P43_MultiplyStrings.java
+++ leetcode-string/src/main/java/edu.university.leetcode.string/medium/P43_MultiplyStrings.java
@@ -2,6 +2,18 @@ package edu.university.leetcode.string.medium;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 43. Multiply Strings
+ * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2,
+ * also represented as a string.
+ * Example 1: Input: num1 = "2", num2 = "3" Output: "6"
+ * Example 2: Input: num1 = "123", num2 = "456" Output: "56088"
+ * Note:
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ */
 @Slf4j
 public class P43_MultiplyStrings {
 
