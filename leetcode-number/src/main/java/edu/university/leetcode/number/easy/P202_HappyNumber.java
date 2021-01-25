968c64b75dd2fadab000c7051f374060f4b2409d
lillian <zheng.lillian@gmail.com>
2021-01-24 16:43:50 -0800

    202. Happy Number

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/P202_HappyNumber.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P202_HappyNumber.java
@@ -1,7 +1,5 @@
 package edu.university.leetcode.number.easy;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * 202. Happy Number
  * Write an algorithm to determine if a number is "happy".
@@ -16,16 +14,32 @@ import lombok.extern.slf4j.Slf4j;
  * 6^2 + 8^2 = 100
  * 1^2 + 0^2 + 0^2 = 1
  * unhappy number circle 4 → 16 → 37 → 58 → 89 → 145 → 42 → 20 → 4
+ * 3 -> 9 -> 81 -> 65 -> 61 -> 37 -> 58 -> 89 -> 145 -> 42 -> 20 -> 4
  * happy number 1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100
+ * 7 -> 49 -> 97 -> 130 -> 10 -> 1
+ * 10 -> 1
+ * 13 -> 10
+ * 19 -> 10
+ * 23 -> 13
+ * 28 -> 68 -> 100 -> 1
+ * 31 -> 10
+ * 32 -> 13
+ * 44 -> 32
+ * 49 -> 97 -> 130
+ * 70 -> 49
+ * 79 -> 130
+ * 82 -> 68
+ * 86 -> 100 -> 1
+ * 91 -> 10
+ * 94 -> 97
  */
-@Slf4j
 public class P202_HappyNumber {
 
     public static boolean isHappy(int n) {
         while(n >= 10){
             n = getNumber(n);
         }
-        log.info("n={}", n);
+        System.out.print("n=" + n);
         return (n == 1 || n == 7);
     }
 
@@ -35,22 +49,30 @@ public class P202_HappyNumber {
             result += Math.pow(n % 10, 2);
             n /= 10;
         }
-        log.info(" -> {}", result);
+        System.out.print(" -> " + result);
         return result;
     }
 
-    public boolean isHappyNumber(int n) {
+    public static boolean isHappyNumber(int n) {
         // 4 → 16 → 37 → 58 → 89 → 145 → 42 → 20 → 4
-        while (n != 1 || n != 4) {
-            int tmp = n;
-            n = 0;
-            while (tmp > 0) {
-                int d = tmp % 10;
-                n += d * d;
-                tmp /= 10;
+        while(n != 1){
+            n = getNext(n);
+            if(n == 4){
+                return false;
             }
         }
-        return n == 1;
+        return true;
+    }
+
+    private static int getNext(int n){
+        int d = 0;
+        System.out.print(n + " -> ");
+        while(n != 0){
+            d += Math.pow(n % 10, 2);
+            n /= 10;
+        }
+        System.out.println("d=" + d);
+        return d;
     }
 
 }
--- leetcode-number/src/test/java/edu/university/leetcode/number/easy/P202_HappyNumberTest.java
+++ leetcode-number/src/test/java/edu/university/leetcode/number/easy/P202_HappyNumberTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class P202_HappyNumberTest {
     @Test
     public void isHappy() throws Exception {
-        boolean result = P202_HappyNumber.isHappy(16);
+        boolean result = P202_HappyNumber.isHappyNumber(3);
         assertEquals(false, result);
     }
 
