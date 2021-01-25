3be00b18ec101a0b6eee1004234e3287c8460e39
lillian <zheng.lillian@gmail.com>
2021-01-24 17:00:49 -0800

    172. Factorial Trailing Zeroes

--- leetcode-string/src/main/java/edu.university.leetcode.string/easy/P172_FactorialTrailingZeroes.java
+++ leetcode-string/src/main/java/edu.university.leetcode.string/easy/P172_FactorialTrailingZeroes.java
@@ -4,14 +4,11 @@ package edu.university.leetcode.string.easy;
  * 172. Factorial Trailing Zeroes
  * Given an integer n, return the number of trailing zeroes in n!.
  * Follow up: Could you write a solution that works in logarithmic time complexity?
- * Example 1: Input: n = 3
- * Output: 0
+ * Example 1: Input: n = 3 Output: 0
  * Explanation: 3! = 6, no trailing zero.
- * Example 2: Input: n = 5
- * Output: 1
+ * Example 2: Input: n = 5 Output: 1
  * Explanation: 5! = 120, one trailing zero.
- * Example 3: Input: n = 0
- * Output: 0
+ * Example 3: Input: n = 0 Output: 0
  * Constraints: 0 <= n <= 104
  */
 public class P172_FactorialTrailingZeroes {
--- leetcode-string/src/test/java/edu/university/leetcode/string/easy/P172_FactorialTrailingZeroesTest.java
+++ leetcode-string/src/test/java/edu/university/leetcode/string/easy/P172_FactorialTrailingZeroesTest.java
@@ -9,8 +9,13 @@ public class P172_FactorialTrailingZeroesTest {
 
     @Test
     public void testTrailingZeroes() {
-        assertEquals(0, trailingZeroes(3));
-        assertEquals(1, trailingZeroes(5));
         assertEquals(0, trailingZeroes(0));
+        assertEquals(0, trailingZeroes(3));
+        assertEquals(0, trailingZeroes(4));
+        assertEquals(1, trailingZeroes(5));
+        assertEquals(2, trailingZeroes(10));
+        assertEquals(3, trailingZeroes(15));
+        assertEquals(4, trailingZeroes(20));
+        assertEquals(24, trailingZeroes(104));
     }
 }
