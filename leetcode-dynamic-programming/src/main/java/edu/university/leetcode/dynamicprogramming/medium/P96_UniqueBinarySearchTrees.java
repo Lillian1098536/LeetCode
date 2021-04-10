d25cf605f447d5159a212232fb4006ea0dcef7f5
lillian <zheng.lillian@gmail.com>
2021-04-10 15:44:13 -0700

    96. Unique Binary Search Trees

---
+++ leetcode-dynamic-programming/src/main/java/edu/university/leetcode/dynamicprogramming/medium/P96_UniqueBinarySearchTrees.java
@@ -0,0 +1,23 @@
+package edu.university.leetcode.dynamicprogramming.medium;
+
+/**
+ * 96. Unique Binary Search Trees
+ * Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+ * Example 1: Input: n = 3 Output: 5
+ * Example 2: Input: n = 1 Output: 1
+ * Constraints: 1 <= n <= 19
+ */
+public class P96_UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                        // n-i in right * i-1 in left
+                dp[i] = dp[i] + (dp[i - j] * dp[j - 1]);
+            }
+        }
+        return dp[n];
+    }
+}
