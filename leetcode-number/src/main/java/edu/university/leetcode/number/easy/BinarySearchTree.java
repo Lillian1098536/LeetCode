a3ca3a546c2bfbee1676f9e0a494515e7f265443
lillian <zheng.lillian@gmail.com>
2021-04-25 19:40:16 -0700

    Binary Search Tree Traversal

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/BinarySearchTree.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/BinarySearchTree.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 /**
  * Binary Search Tree Traversal
- * pre-order
- * in-order
- * post-order
+ * pre-order: current node -> left child -> right child
+ * in-order: left child -> current node -> right child
+ * post-order: left child -> right child -> current node
  */
 public class BinarySearchTree {
 
+    /**
+     * Time complexity O(n)
+     */
     public List<Integer> orderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         preOrder(root, result);
@@ -52,12 +55,46 @@ public class BinarySearchTree {
         return;
     }
 
+    /**
+     * Time complexity O(n^2)
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
+            return false;
+        } else {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    /**
+     * Time complexity O(n)
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+    public TreeNode sortedArrayToBST(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int m = (l + r) / 2;
+        TreeNode node = new TreeNode(nums[m]);
+        node.left = sortedArrayToBST(nums, l, m - 1);
+        node.right = sortedArrayToBST(nums, m + 1, r);
+        return node;
+    }
+    
     public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int x) {
             this.val = x;
+            this.left = this.right = null;
         }
     }
 }
