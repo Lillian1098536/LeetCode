b2c5d5de090c3250f7b2cab88bafb9d6c4c1bf01
lillian <zheng.lillian@gmail.com>
2021-05-01 16:56:48 -0700

    295. Find Median from Data Stream

---
+++ leetcode-string/src/main/java/edu/university/leetcode/string/hard/P295_FindMedianFromDataStream.java
@@ -0,0 +1,66 @@
+package edu.university.leetcode.string.hard;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 295. Find Median from Data Stream
+ * The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+ * For example, for arr = [2,3,4], the median is 3.
+ * For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+ * Implement the MedianFinder class:
+ * MedianFinder() initializes the MedianFinder object.
+ * void addNum(int num) adds the integer num from the data stream to the data structure.
+ * double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ * Example 1:
+ * Input ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+ * [[], [1], [2], [], [3], []]
+ * Output [null, null, null, 1.5, null, 2.0]
+ * Explanation
+ * MedianFinder medianFinder = new MedianFinder();
+ * medianFinder.addNum(1);    // arr = [1]
+ * medianFinder.addNum(2);    // arr = [1, 2]
+ * medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+ * medianFinder.addNum(3);    // arr[1, 2, 3]
+ * medianFinder.findMedian(); // return 2.0
+ * Constraints:
+ * -105 <= num <= 105
+ * There will be at least one element in the data structure before calling findMedian.
+ * At most 5 * 104 calls will be made to addNum and findMedian.
+ */
+public class P295_FindMedianFromDataStream {
+    static class MedianFinder {
+        PriorityQueue<Integer> minHeap;
+        PriorityQueue<Integer> maxHeap;
+
+        public MedianFinder() {
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        }
+
+        public void addNum(int num) {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+            if (minHeap.size() < maxHeap.size()) {
+                minHeap.offer(maxHeap.poll());
+            }
+        }
+
+        public double findMedian() {
+            if (minHeap.size() > maxHeap.size()) {
+                return minHeap.peek();
+            } else {
+                return (minHeap.peek() + maxHeap.peek()) / 2.0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MedianFinder test = new MedianFinder();
+        test.addNum(1);
+        test.addNum(2);
+        System.out.println("Median " + test.findMedian());
+        test.addNum(3);
+        System.out.println("Median " + test.findMedian());
+    }
+}
