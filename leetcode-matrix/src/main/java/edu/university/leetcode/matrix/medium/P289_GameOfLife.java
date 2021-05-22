861701e47931b3ab2144affe619eca13dfedc8ac
lillian <zheng.lillian@gmail.com>
2021-05-21 20:44:04 -0700

    289. Game of Life

---
+++ leetcode-matrix/src/main/java/edu/university/leetcode/matrix/medium/P289_GameOfLife.java
@@ -0,0 +1,57 @@
+package edu.university.leetcode.matrix.medium;
+
+/**
+ * 289. Game of Life
+ * According to Wikipedia's article: "The Game of Life, also known simply as Life,
+ * is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+ * The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0).
+ * Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+ * Any live cell with fewer than two live neighbors dies as if caused by under-population.
+ * Any live cell with two or three live neighbors lives on to the next generation.
+ * Any live cell with more than three live neighbors dies, as if by over-population.
+ * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+ * The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+ * https://zh.wikipedia.org/wiki/康威生命游戏
+ * 生命遊戲中，對於任意細胞，規則如下：
+ * 每個細胞有兩種狀態 - 存活或死亡，每個細胞與以自身為中心的周圍八格細胞產生互動（1為存活，0為死亡）
+ * 當前細胞為存活狀態時，當周圍的存活細胞低於2個時（不包含2個），該細胞變成死亡狀態。（模擬生命數量稀少）
+ * 當前細胞為存活狀態時，當周圍有2個或3個存活細胞時，該細胞保持原樣。
+ * 當前細胞為存活狀態時，當周圍有超過3個存活細胞時，該細胞變成死亡狀態。（模擬生命數量過多）
+ * 當前細胞為死亡狀態時，當周圍有3個存活細胞時，該細胞變成存活狀態。（模擬繁殖）
+ * 可以把最初的細胞結構定義為種子，當所有在種子中的細胞同時被以上規則處理後，可以得到第一代細胞圖。按規則繼續處理當前的細胞圖，可以得到下一代的細胞圖，周而復始。
+ * Example 1:
+ * Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+ * Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+ *
+ */
+public class P289_GameOfLife {
+    public void gameOfLife(int[][] board) {
+        if (board.length == 0 || board[0].length == 0) return;
+        int m = board.length, n = board[0].length;
+        int[] dx = {1, 1, 1, 0, -1, -1, -1, 0}, dy = {1, 0, -1, -1, -1, 0, 1, 1};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int count = 0;
+                for (int k = 0; k < 8; k++) {
+                    int x = i + dx[k], y = j + dy[k];
+                    if (x >= 0 && x < m && y >= 0 && y < n && (board[x][y] == 1 || board[x][y] == 3)) {
+                        count++;
+                    }
+                }
+                if (board[i][j] == 1 && (count < 2 || count > 3)) {
+                    //當前細胞為存活狀態時，當周圍的存活細胞低於2個時（不包含2個），該細胞變成死亡狀態。（模擬生命數量稀少）
+                    //當前細胞為存活狀態時，當周圍有超過3個存活細胞時，該細胞變成死亡狀態。（模擬生命數量過多）
+                    board[i][j] = 3; //from 1 to 0
+                } else if (board[i][j] == 0 && count == 3) {
+                    //當前細胞為死亡狀態時，當周圍有3個存活細胞時，該細胞變成存活狀態。（模擬繁殖）
+                    board[i][j] = 2; //from 0 to 1
+                } // * 當前細胞為存活狀態時，當周圍有2個或3個存活細胞時，該細胞保持原樣。
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = board[i][j] == 0 || board[i][j] == 3 ? 0 : 1;
+            }
+        }
+    }
+}
