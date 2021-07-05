d91a1f6036cabc414b699543a13e60662668b8c9
lillian <zheng.lillian@gmail.com>
2021-07-04 21:43:18 -0700

    1137. N-th Tribonacci Number

---
+++ leetcode-dynamic-programming/src/test/java/edu/university/leetcode/dynamicprogramming/easy/P1137_NthTribonacciNumberTest.java
@@ -0,0 +1,15 @@
+package edu.university.leetcode.dynamicprogramming.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P1137_NthTribonacciNumberTest {
+    P1137_NthTribonacciNumber test = new P1137_NthTribonacciNumber();
+    @Test
+    public void tribonacci() {
+        assertEquals(2, test.tribonacci(3));
+        assertEquals(4, test.tribonacci(4));
+        assertEquals(1389537, test.tribonacci(25));
+    }
+}
