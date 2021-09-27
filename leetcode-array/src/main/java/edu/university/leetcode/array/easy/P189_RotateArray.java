27dbb260de71a27e45710147b71f7eadd9c9dc75
lillian <zheng.lillian@gmail.com>
2021-09-26 22:44:50 -0700

    189. Rotate Array

--- leetcode-array/src/main/java/edu/university/leetcode/array/easy/P189_RotateArray.java
+++ leetcode-array/src/main/java/edu/university/leetcode/array/easy/P189_RotateArray.java
@@ -1,5 +1,8 @@
 package edu.university.leetcode.array.easy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 189. Rotate Array
  * Rotate an array of n elements to the right by k steps.
@@ -8,7 +11,12 @@ package edu.university.leetcode.array.easy;
  * Hint: Could you do it in-place with O(1) extra space?
  */
 public class P189_RotateArray {
+    private static final Logger LOG = LoggerFactory.getLogger(P189_RotateArray.class);
 
+    /**
+     * Time complexity  O(N)
+     * Space complexity O(1)
+     */
     public void rotate(int[] nums, int k) {
         int length = nums.length;
         k %= length;
@@ -24,6 +32,35 @@ public class P189_RotateArray {
             nums[start++] = nums[end];
             nums[end--] = tmp;
         }
+    }
+
+    /**
+     * n = ++i increments a and returns that value
+     * n = 1, i = 1
+     * while n = i++ returns a's value and then increments a
+     * n = 0, i = 1
+     */
+    public static void rotateArray(int[] nums, int k) {
+        if (nums == null || (k %= nums.length) == 0) return;
+        int n = nums.length, start = 0, index = 0, pre = 0, cur = nums[0];
+        for (int i = 0; i < n; ++i) {
+            LOG.info("i={} nums={}", i, nums);
+            pre = cur;
+            index = (index + k) % n;
+            cur = nums[index];
+            nums[index] = pre;
+            LOG.info("start={} index={} pre={} cur={}", start, index, pre, cur);
+            if(index == start) {
+                index = ++start;
+                cur = nums[index];
+                LOG.info(">>> index={} start={} cur={} nums={}", index, start, cur, nums);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotateArray(nums, 3);
     }
 
 }
