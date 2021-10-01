e41d24786293a1e15c59ccc3f39e3e0b6a18d09b
lillian <zheng.lillian@gmail.com>
2021-09-30 20:52:15 -0700

    303. Range Sum Query - Immutable

--- leetcode-array/src/main/java/edu/university/leetcode/array/easy/P303_RangeSumQueryImmutable.java
+++ leetcode-array/src/main/java/edu/university/leetcode/array/easy/P303_RangeSumQueryImmutable.java
@@ -13,18 +13,36 @@ package edu.university.leetcode.array.easy;
  */
 public class P303_RangeSumQueryImmutable {
 
-    int[] nums;
-
+    private int[] nums;
     public P303_RangeSumQueryImmutable(int[] nums) {
         this.nums = nums;
     }
-
     public int sumRange(int i, int j) {
         int sum = 0;
         for (int k = i; k <= j; k++) {
             sum +=  nums[k];
         }
         return sum;
+    }
+
+    static class NumArray {
+        private int[] dp;
+        public NumArray(int[] nums) {
+            dp = new int[nums.length + 1];
+            for (int i = 1; i <= nums.length; i++) {
+                dp[i] = dp[i - 1] + nums[i - 1];
+            }
+        }
+        public int sumRange(int i, int j) {
+            return dp[j + 1] - dp[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 0, 3, -5, 2, -1};
+        NumArray test = new NumArray(nums);
+        System.out.println(test.sumRange(0, 1));
+        System.out.println(test.sumRange(1, 2));
     }
 
 }
