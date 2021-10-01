6c875c02f31cca7bcd679bbf760af8574194b4cb
lillian <zheng.lillian@gmail.com>
2021-09-30 20:32:37 -0700

    252. Meeting Rooms

--- leetcode-array/src/test/java/edu/university/leetcode/array/easy/P252_MeetingRoomsTest.java
+++ leetcode-array/src/test/java/edu/university/leetcode/array/easy/P252_MeetingRoomsTest.java
@@ -6,13 +6,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class P252_MeetingRoomsTest {
-
+    P252_MeetingRooms test = new P252_MeetingRooms();
     @Test
     public void canAttendMeetings() {
         Interval[] intervals = new Interval[3];
         intervals[0] = new Interval(0, 30);
         intervals[1] = new Interval(5, 20);
         intervals[2] = new Interval(15, 20);
-        assertTrue(!P252_MeetingRooms.canAttendMeetings(intervals));
+        assertTrue(!test.canAttendMeetings(intervals));
+    }
+    @Test
+    public void canAttendMeetingsTrue() {
+        Interval[] intervals = new Interval[3];
+        intervals[0] = new Interval(0, 10);
+        intervals[1] = new Interval(10, 20);
+        intervals[2] = new Interval(20, 30);
+        assertTrue(test.canAttendMeetings(intervals));
     }
 }
