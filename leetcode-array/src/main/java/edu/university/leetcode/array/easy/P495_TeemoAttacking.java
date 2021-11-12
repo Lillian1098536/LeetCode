c284b480e90b029788cedc443f4baf292d11bcd1
lillian <zheng.lillian@gmail.com>
2021-11-11 17:58:49 -0800

    495. Teemo Attacking

--- leetcode-array/src/main/java/edu/university/leetcode/array/easy/P495_TeemoAttacking.java
+++ leetcode-array/src/main/java/edu/university/leetcode/array/easy/P495_TeemoAttacking.java
@@ -28,7 +28,7 @@ public class P495_TeemoAttacking {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         if (timeSeries == null || timeSeries.length == 0) return 0;
         int result = 0;
-        for (int i = 1; i < timeSeries.length; ++i) {
+        for (int i = 1; i < timeSeries.length; i++) {
             int diff = timeSeries[i] - timeSeries[ i - 1];
             result += (diff < duration) ? diff : duration;
         }
