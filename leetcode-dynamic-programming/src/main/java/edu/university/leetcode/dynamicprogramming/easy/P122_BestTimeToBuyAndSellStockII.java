3bc8fe3cc5af820dcec808a9bd229f13ab56a219
lillian <zheng.lillian@gmail.com>
2022-01-04 20:39:27 -0800

    122. Best Time to Buy and Sell Stock II

--- leetcode-dynamic-programming/src/main/java/edu/university/leetcode/dynamicprogramming/easy/P122_BestTimeToBuyAndSellStockII.java
+++ leetcode-dynamic-programming/src/main/java/edu/university/leetcode/dynamicprogramming/easy/P122_BestTimeToBuyAndSellStockII.java
@@ -2,14 +2,26 @@ package edu.university.leetcode.dynamicprogramming.easy;
 
 /**
  * 122. Best Time to Buy and Sell Stock II
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * Design an algorithm to find the maximum profit. You may complete as many transactions as you like
- * (ie, buy one and sell one share of the stock multiple times).
- * However, you may not engage in multiple transactions at the same time
- * (ie, you must sell the stock before you buy again).
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ * On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+ * However, you can buy it then immediately sell it on the same day.
+ * Find and return the maximum profit you can achieve.
+ * Example 1: Input: prices = [7,1,5,3,6,4] Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ * Total profit is 4 + 3 = 7.
+ * Example 2: Input: prices = [1,2,3,4,5] Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ * Total profit is 4.
+ * Example 3: Input: prices = [7,6,4,3,1] Output: 0
+ * Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
  */
 public class P122_BestTimeToBuyAndSellStockII {
-
+    /**
+     * greedy
+     * Time complexity O(n)
+     * Space complexity O(1)
+     */
     public int maxProfit(int[] prices) {
         int max = 0;
         if (prices == null || prices.length == 0) {
@@ -22,5 +34,4 @@ public class P122_BestTimeToBuyAndSellStockII {
         }
         return max;
     }
-
 }
