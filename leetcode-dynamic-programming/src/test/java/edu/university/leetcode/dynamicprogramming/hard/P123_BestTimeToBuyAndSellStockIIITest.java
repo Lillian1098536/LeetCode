fee636104bdec906c638460eb7ed78c319fbe4a4
lillian <zheng.lillian@gmail.com>
2022-01-06 18:49:12 -0800

    123. Best Time to Buy and Sell Stock III

--- leetcode-dynamic-programming/src/test/java/edu/university/leetcode/dynamicprogramming/hard/P123_BestTimeToBuyAndSellStockIIITest.java
+++ leetcode-dynamic-programming/src/test/java/edu/university/leetcode/dynamicprogramming/hard/P123_BestTimeToBuyAndSellStockIIITest.java
@@ -9,7 +9,7 @@ public class P123_BestTimeToBuyAndSellStockIIITest {
     @Test
     public void maxProfit() {
         assertEquals(6, test.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
-//        assertEquals(4, test.maxProfit(new int[]{1, 2, 3, 4, 5}));
-//        assertEquals(0, test.maxProfit(new int[]{7, 6, 4, 3, 1}));
+        assertEquals(4, test.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(0, test.maxProfit(new int[]{7, 6, 4, 3, 1}));
     }
 }
