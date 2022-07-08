19e0059f518db2a977592a860c024ba9fe7597ac
lillian <zheng.lillian@gmail.com>
2022-07-07 23:14:57 -0700

    804. Unique Morse Code Words

--- leetcode-hash-table/src/test/java/edu/university/leetcode/hashtable/easy/P804_UniqueMorseCodeWordsTest.java
+++ leetcode-hash-table/src/test/java/edu/university/leetcode/hashtable/easy/P804_UniqueMorseCodeWordsTest.java
@@ -1,13 +1,13 @@
 package edu.university.leetcode.hashtable.easy;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class P804_UniqueMorseCodeWordsTest {
     P804_UniqueMorseCodeWords test = new P804_UniqueMorseCodeWords();
     @Test
     public void uniqueMorseRepresentations() {
         assertEquals(2, test.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"}));
+        assertEquals(1, test.uniqueMorseRepresentations(new String[]{"a"}));
     }
 }
