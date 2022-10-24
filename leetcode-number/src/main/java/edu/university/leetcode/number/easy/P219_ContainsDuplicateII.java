0854c8cadb3dbe56f2b5c465b8a89bc3d765cf9d
lillian <zheng.lillian@gmail.com>
2022-10-23 21:43:50 -0700

    219. Contains Duplicate II

--- leetcode-number/src/main/java/edu/university/leetcode/number/easy/P219_ContainsDuplicateII.java
+++ leetcode-number/src/main/java/edu/university/leetcode/number/easy/P219_ContainsDuplicateII.java
@@ -7,17 +7,20 @@ import java.util.Set;
 
 /**
  * 219. Contains Duplicate II
- * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array
- * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ * Given an integer array nums and an integer k,
+ * return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
  * Example 1: Input: nums = [1,2,3,1], k = 3 Output: true
  * Example 2: Input: nums = [1,0,1,1], k = 1 Output: true
  * Example 3: Input: nums = [1,2,3,1,2,3], k = 2 Output: false
-
+ * Constraints:
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * 0 <= k <= 105
  */
 @Slf4j
 public class P219_ContainsDuplicateII {
 
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (i > k) {
--- leetcode-number/src/test/java/edu/university/leetcode/number/easy/P219_ContainsDuplicateIITest.java
+++ leetcode-number/src/test/java/edu/university/leetcode/number/easy/P219_ContainsDuplicateIITest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class P219_ContainsDuplicateIITest {
+    P219_ContainsDuplicateII test = new P219_ContainsDuplicateII();
+
     @Test
     public void containsNearbyDuplicate() throws Exception {
-        assertTrue(P219_ContainsDuplicateII.containsNearbyDuplicate(new int[] {1, 0, 1, 1}, 1));
+        assertTrue(test.containsNearbyDuplicate(new int[] {1,2,3,1}, 3));
+        assertTrue(test.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+        assertFalse(test.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
     }
 
 }
