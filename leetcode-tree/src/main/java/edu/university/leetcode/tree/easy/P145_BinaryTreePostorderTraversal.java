625b0a9c65bf3b9c3570c7a3c6e8dacb027da288
lillian <zheng.lillian@gmail.com>
2023-12-01 19:51:20 -0800

    145. Binary Tree Postorder Traversal

--- leetcode-tree/src/main/java/edu/university/leetcode/tree/easy/P145_BinaryTreePostorderTraversal.java
+++ leetcode-tree/src/main/java/edu/university/leetcode/tree/easy/P145_BinaryTreePostorderTraversal.java
@@ -1,2 +1,45 @@
-package edu.university.leetcode.tree.easy;public class P145_BinaryTreePostorderTraversal {
+package edu.university.leetcode.tree.easy;
+
+import static edu.university.leetcode.tree.easy.P100_SameTree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 145. Binary Tree Postorder Traversal
+ * Given the root of a binary tree, return the postorder traversal of its nodes' values.
+ * Example 1: Input: root = [1,null,2,3] Output: [3,2,1]
+ *   1
+ *    \
+ *    2
+ *   /
+ *  3
+ * Example 2: Input: root = [] Output: []
+ * Example 3: Input: root = [1] Output: [1]
+ * Constraints:
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ */
+public class P145_BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderTraversal(root, result);
+        return result;
+    }
+
+    public void postorderTraversal(TreeNode root, List<Integer> result ) {
+        if (root == null) return;
+        postorderTraversal(root.left, result);
+        postorderTraversal(root.right, result);
+        result.add(root.val);
+    }
+
+    public static void main(String[] args) {
+        P145_BinaryTreePostorderTraversal test = new P145_BinaryTreePostorderTraversal();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(test.postorderTraversal(root));
+    }
+
 }
